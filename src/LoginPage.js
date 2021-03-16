0ecbaff76c5fe9fa6bb594a03d736181715d0a26
stphncrt <cretestephen@gmail.com>
2021-03-16 20:38:03 +0300

    services.json changed

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import {View, Text, TextInput, Button} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const LoginPage = () => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const signIn = () => {};
+  const signUp = () => {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   return (
     <View>
       <Text
@@ -19,7 +36,10 @@ const LoginPage = () => {
           margin: 5,
           boderRadius: 5,
         }}>
-        <TextInput placeholder="Type your mail.." />
+        <TextInput
+          placeholder="Type your mail.."
+          onChangeText={text => setmail(text)}
+        />
       </View>
       <View
         style={{
@@ -27,13 +47,16 @@ const LoginPage = () => {
           margin: 5,
           boderRadius: 5,
         }}>
-        <TextInput placeholder="Type your password.." />
+        <TextInput
+          placeholder="Type your password.."
+          onChangeText={text => setPassword(text)}
+        />
       </View>
       <View style={{margin: 5}}>
-        <Button title="Sign In" />
+        <Button title="Sign In" onPress={signIn} />
       </View>
       <View style={{margin: 5}}>
-        <Button title="Sign Up" />
+        <Button title="Sign Up" onpress={signUp} />
       </View>
     </View>
   );
